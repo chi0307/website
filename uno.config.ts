6fa71ed7d094afe82dfc8700bbb05502956fd9b0
Junchi <benson26365156@gmail.com>
2024-09-01 14:35:30 +0800

    feat: add basic colors

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,21 @@
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  theme: {
+    colors: {
+      primary: {
+        100: '#71604D',
+        90: '#84745B',
+        80: '#A39481',
+        70: '#B8AB9A',
+        60: '#C8BCAC',
+        50: '#DAD1C2',
+        40: '#E2DDD1',
+        30: '#EEE8DC',
+        20: '#F9F4E9'
+      },
+      secondary: '#C5C7BA',
+      tertiary: '#CCBAB1'
+    }
+  }
 })
