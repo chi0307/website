d11a52b2f527248e9924c179d77683adc62da277
Junchi <benson26365156@gmail.com>
2024-09-22 18:55:20 +0800

    feat: add root page style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
-  <RouterView />
+  <div class="w-100vw relative bg-white h-100vh">
+    <header
+      class="z-100 top-0 left-0 sticky w-full bg-white"
+      :style="{
+        height: headerHeight
+      }"
+    ></header>
+    <RouterView
+      class="w-full"
+      :style="{
+        height: `calc(100% - ${headerHeight})`
+      }"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+
+const headerHeight = '80px'
 </script>
+<style>
+.flex-col {
+  @apply flex;
+}
+</style>
